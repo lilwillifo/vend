5e0c7dee7ff7fce6636751de1b0840c4380ff0f7
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-06 10:06:18 -0600

    first feature test

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,15 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+  it 'they see the name of all of the snacks associated with that vending machine along with their price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    visit machine_path(dons)
+
+    dons.snacks.each do
+      expect(page).to have_content(snack.type)
+      expect(page).to have_content(snack.price)
+    end
+  end
 end
